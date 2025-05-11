a5ec1d6e099cb404c224a62becb8bfac273a1f5f
tivrfoa <lescoutinhovr@gmail.com>
2025-05-11 13:55:43 -0300

    move Grok css to its js

--- src/Grok3.js
+++ src/Grok3.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import "./grok3.css";
 
 function Square({ value, onSquareClick }) {
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./Gemini2.5Pro";
+// import App from "./Gemini2.5Pro";
+import App from "./Grok3";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
